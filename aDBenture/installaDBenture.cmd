98fba1c5e803d91541cb12f8b85e22ed6f3f849f
Nicole <nicolebu@edu.aau.at>
2021-07-27 13:21:03 +0200

    gui optimizations and cmd files

---
+++ aDBenture/installaDBenture.cmd
@@ -0,0 +1,8 @@
+start cmd /c pip install Django==3.2.5
+pip install django-widget-tweaks
+pip install pillow
+pip install psycopg2-binary
+pip install django-celery
+python manage.py makemigrations
+python manage.py migrate
+exit
